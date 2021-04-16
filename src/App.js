3dd7efabdfd8f5ee92709d62d171a763627b90f2
Will Mallett <mallett002@gmail.com>
2021-04-16 06:28:33 -0500

    updating package and moving App into src dir

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Platform} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
@@ -10,14 +11,36 @@ import {tabScreenOptions} from './components/nav/helpers';
 
 const Tab = createBottomTabNavigator();
 
+const getTabBarStyles = () => {
+  const styles = {
+    borderTopWidth: 0,
+    height: 70,
+    paddingTop: 9,
+    paddingBottom: 9,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowRadius: 20,
+    shadowOffset: {width: 0, height: 0}
+  };  
+
+  if (Platform.OS === 'ios') {
+    styles.height = 90;
+    styles.paddingBottom = 23;
+  }
+
+  return styles;
+};
+
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator
         screenOptions={tabScreenOptions}
+        style={{padding: 10}}
         tabBarOptions={{
           activeTintColor: 'tomato',
           inactiveTintColor: 'gray',
+          style: getTabBarStyles()
         }}
       >
         <Tab.Screen name='Home' component={HomeScreen} />
