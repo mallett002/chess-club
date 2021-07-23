a5260b132fa76c9adca6a8751fd7ff579b49d3ce
Will Mallett <mallett002@gmail.com>
2021-07-23 07:48:48 -0500

    spiking on creating a game, getting network errors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import {Platform} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { Platform, AppRegistry } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+// import { persistCache } from 'apollo3-cache-persist'
 
 import HomeScreen from './pages/home/home-screen';
-// import GamesScreen from './pages/games/games-screen';
+import GamesScreen from './pages/games/games-screen';
 import Board from './pages/games/board';
 import ProfileScreen from './pages/profile/profile-screen';
 import ChatsScreen from './pages/chats/chats-screen';
-import {tabScreenOptions} from './components/nav/helpers';
+import { tabScreenOptions } from './components/nav/helpers';
+
+const client = new ApolloClient({
+  uri: 'localhost:4000',
+  cache: new InMemoryCache()
+});
 
 const Tab = createBottomTabNavigator();
 
@@ -21,8 +28,8 @@ const getTabBarStyles = () => {
     shadowColor: '#000',
     shadowOpacity: 0.1,
     shadowRadius: 20,
-    shadowOffset: {width: 0, height: 0}
-  };  
+    shadowOffset: { width: 0, height: 0 }
+  };
 
   if (Platform.OS === 'ios') {
     styles.height = 90;
@@ -32,12 +39,12 @@ const getTabBarStyles = () => {
   return styles;
 };
 
-export default function App() {
-  return (
+const App = () => (
+  <ApolloProvider client={client}>
     <NavigationContainer>
       <Tab.Navigator
         screenOptions={tabScreenOptions}
-        style={{padding: 10}}
+        style={{ padding: 10 }}
         tabBarOptions={{
           activeTintColor: 'tomato',
           inactiveTintColor: 'gray',
@@ -45,10 +52,14 @@ export default function App() {
         }}
       >
         <Tab.Screen name='Home' component={HomeScreen} />
-        <Tab.Screen name='Games' component={Board} />
+        <Tab.Screen name='Games' component={GamesScreen} />
         <Tab.Screen name='Profile' component={ProfileScreen} />
         <Tab.Screen name='Chats' component={ChatsScreen} />
       </Tab.Navigator>
     </NavigationContainer>
-  );
-}
+  </ApolloProvider >
+);
+
+AppRegistry.registerComponent('ChessClub', () => App);
+
+export default App;
--- src/pages/games/games-screen.js
+++ src/pages/games/games-screen.js
@@ -1,12 +1,51 @@
+import { gql, useMutation } from '@apollo/client';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
+
+const CREATE_GAME_MUTATION = gql`
+  mutation createGame($playerOne: ID!, $playerTwo: ID!) {
+    createGame(playerOne: $playerOne, playerTwo: $playerTwo) {
+      gameId
+      moves {
+        color
+        from
+        to
+        flags
+        piece
+        san
+      }
+      players
+      positions {
+        type
+        color
+        label
+      }
+      turn
+    }
+  }`;
 
 export default function GamesScreen() {
+  const [ createGameMutation, {data} ] = useMutation(CREATE_GAME_MUTATION);
+
+  console.log({dataIs: data});
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Games!</Text>
       <Text>Invitations</Text>
       <Text>Current Games</Text>
+      <Button
+        onPress={async () => {
+          await createGameMutation({
+            variables: {
+              playerOne: "some-guid-1",
+              playerTwo: "some-guid-2"
+             }
+          });
+        }}
+        title='Create Game (for now)'
+        color='#841584'
+      />
     </View>
   );
 }
