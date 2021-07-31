dc6ce8deb8054b81555d2447e23fad24e2cf54c9
Will Mallett <mallett002@gmail.com>
2021-07-31 08:18:49 -0500

    if error, show that

--- src/pages/games/current-games.js
+++ src/pages/games/current-games.js
@@ -8,28 +8,35 @@ const styles = StyleSheet.create({
   }
 });
 
-const CurrentGames = ({ games, loading, error }) => (
-  <View>
-    <Text>{'Current Games'}</Text>
-    {error && <Text>{'There was an error loading games.'}</Text>}
-    {loading && <Text>{'Loading current games...'}</Text>}
-    {games && games.getGames && games.getGames.length ?
-      <FlatList
-        data={games.getGames}
-        renderItem={({ item }) => {
-          return (
-            <View style={styles.gameItem}>
-              <Text>{item.gameId}</Text>
-              <Text>{item.opponent}</Text>
-              <Text>{item.turn}</Text>
-            </View>
-          )
-        }}
-        keyExtractor={game => game.gameId}
-      /> :
-      <Text>{"You currently don't have any games"}</Text>
-    }
-  </View>
-);
+const CurrentGames = ({ games, loading, error }) => {
+  if (error) {
+    return (
+      <View><Text>{'There was an error loading games.'}</Text></View>
+    )
+  }
+
+  return (
+    <View>
+      <Text>{'Current Games'}</Text>
+      {loading && <Text>{'Loading current games...'}</Text>}
+      {games && games.getGames && games.getGames.length ?
+        <FlatList
+          data={games.getGames}
+          renderItem={({ item }) => {
+            return (
+              <View style={styles.gameItem}>
+                <Text>{item.gameId}</Text>
+                <Text>{item.opponent}</Text>
+                <Text>{item.turn}</Text>
+              </View>
+            )
+          }}
+          keyExtractor={game => game.gameId}
+        /> :
+        <Text>{"You currently don't have any games"}</Text>
+      }
+    </View>
+  )
+};
 
 export default CurrentGames;
