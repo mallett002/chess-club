234fdd6cf54ee6236702341495258b6fdcb2bd95
Will Mallett <mallett002@gmail.com>
2021-12-08 06:55:40 -0600

    WIP: working on sign up

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Platform, AppRegistry } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 // import { persistCache } from 'apollo3-cache-persist'
 
@@ -10,13 +11,15 @@ import GamesStack from './pages/games/games-stack';
 import ProfileScreen from './pages/profile/profile-screen';
 import ChatsScreen from './pages/chats/chats-screen';
 import { tabScreenOptions } from './components/nav/helpers';
+import SignUpScreen from './pages/auth';
 
 const client = new ApolloClient({
-  uri: 'http://[machine-id]:4000/graphql',
+  uri: 'http://192.168.0.220:4000/graphql',
   cache: new InMemoryCache()
 });
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
 const getTabBarStyles = () => {
   const styles = {
@@ -38,26 +41,41 @@ const getTabBarStyles = () => {
   return styles;
 };
 
-const App = () => (
-  <ApolloProvider client={client}>
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={tabScreenOptions}
-        style={{ padding: 10 }}
-        tabBarOptions={{
-          activeTintColor: 'tomato',
-          inactiveTintColor: 'gray',
-          style: getTabBarStyles()
-        }}
-      >
-        <Tab.Screen name='Home' component={HomeScreen} />
-        <Tab.Screen name='Games' component={GamesStack} />
-        <Tab.Screen name='Profile' component={ProfileScreen} />
-        <Tab.Screen name='Chats' component={ChatsScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  </ApolloProvider >
-);
+const App = () => {
+  const isSignedIn = false;
+
+  return (
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={tabScreenOptions}
+          style={{ padding: 10 }}
+          tabBarOptions={{
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray',
+            style: getTabBarStyles()
+          }}
+        >
+          {
+            isSignedIn ? (
+              <>
+                <Tab.Screen name='Home' component={HomeScreen} />
+                <Tab.Screen name='Games' component={GamesStack} />
+                <Tab.Screen name='Profile' component={ProfileScreen} />
+                <Tab.Screen name='Chats' component={ChatsScreen} />
+              </>
+            ) : (
+              <>
+                {/* <Stack.Screen name="SignIn" component={SignInScreen} /> */}
+                <Stack.Screen name="SignUp" component={SignUpScreen} />
+              </>
+            )}
+        </Tab.Navigator>
+      </NavigationContainer>
+    </ApolloProvider >
+  );
+}
+
 
 AppRegistry.registerComponent('ChessClub', () => App);
 
