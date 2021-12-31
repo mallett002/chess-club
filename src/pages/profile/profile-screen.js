8986767b81509998b398ceae44bb1ca8f625d3d1
Will Mallett <mallett002@gmail.com>
2021-12-31 07:58:36 -0600

    add some logout functionality

--- src/pages/profile/profile-screen.js
+++ src/pages/profile/profile-screen.js
@@ -1,10 +1,55 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useContext, useEffect, useState } from 'react';
+import { Text, View, StyleSheet, ActivityIndicator } from 'react-native';
 
-export default function ProfileScreen() {
+import { AppContext } from '../../utils/context';
+import { removeTokenFromStorage } from '../../utils/token-utils';
+
+function ProfileScreen() {
+  const [loading, setLoading] = useState(false);
+  const { setAccessToken, setUsername, setPlayerId } = useContext(AppContext);
+
+  const logUserOut = async () => {
+    setLoading(true);
+    setAccessToken('');
+    setUsername('');
+    setPlayerId('');
+    await removeTokenFromStorage();
+    setLoading(false);
+  };
+
+  if (loading) {
+    return (
+      <View style={styles.loader}>
+        <ActivityIndicator
+          color={'red'}
+          size={'large'}
+        />
+      </View>
+    );
+  }
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Profile!</Text>
+    <View style={styles.container}>
+      <Text>{'Profile!'}</Text>
+      <Text
+        onPress={logUserOut}
+        style={styles.logoutText}
+      >
+        {'Logout'}
+      </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  logoutText: {
+    color: 'red'
+  }
+});
+
+export default ProfileScreen;
