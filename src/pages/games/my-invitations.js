12030540aa4c8d3f546dcf6575ac17aa13b20d38
Will Mallett <mallett002@gmail.com>
2022-01-08 06:58:48 -0600

    wip

--- src/pages/games/my-invitations.js
+++ src/pages/games/my-invitations.js
@@ -1,6 +1,7 @@
 import { gql, useQuery } from '@apollo/client';
-import React from 'react';
-import { Text, View, Button, ActivityIndicator } from 'react-native';
+import React, {useState} from 'react';
+import { Text, View, ActivityIndicator } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const INVITATIONS_QUERY = gql`
   query GetInvitations {
@@ -19,6 +20,8 @@ const INVITATIONS_QUERY = gql`
 
 export default function MyInvitations() {
   const { data, error, loading, refetch } = useQuery(INVITATIONS_QUERY);
+  const [showInvitations, setShowInvitations] = useState(true);
+  const [showRequests, setShowRequests] = useState(true);
 
   if (error || !loading && !data || !loading && !data.getInvitations) {
     return (
@@ -47,12 +50,17 @@ export default function MyInvitations() {
   return (
     <View>
       <View>
-        <Text>{'My Invitations'}</Text>
-        {invitations.length ? invitations.map((invite) => <Text>{invite.invitee}</Text>) : <Text>{'No invitations'}</Text>}
+        <TouchableOpacity onPress={() => setShowInvitations(!showInvitations)}>
+          <Text>{'My Invitations'}</Text>
+        </TouchableOpacity>
+        {showInvitations ?
+          invitations.length ?
+            invitations.map((invite) => <Text>{invite.invitee}</Text>) : <Text>{'You currently have not invited anyone'}</Text>
+          : null
+        }
       </View>
       <View>
         <Text>{'Requests'}</Text>
-        {inboundGameRequests.length ? inboundGameRequests.map((invite) => <Text>{invite.invitee}</Text>) : <Text>{'No one wants to play with you...'}</Text>}
       </View>
     </View>
   );
